8139f4a63ed1adc154425c8979eb473fd9de0796
Shashvatb <shashvat.bharti@gmail.com>
2025-09-10 19:56:12 +0200

    added vector embeddings and store

--- RAG.py
+++ RAG.py
@@ -6,6 +6,9 @@
 # query is embedded and queried in vector DB, that response is context for an LLM (with prompt) and that is generated as output 
 
 from langchain_community.document_loaders import TextLoader, WebBaseLoader, PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
 import os
 import bs4
 from dotenv import load_dotenv
@@ -15,7 +18,12 @@ load_dotenv()
 ### from txt
 loader = TextLoader("pokemon.txt")
 text = loader.load()
-print(text)
+# print(text)
+
+### from pdf file
+loader = PyPDFLoader('pokemon.pdf')
+text = loader.load()
+# print(text)
 
 ### from web
 loader = WebBaseLoader(web_paths=("https://en.wikipedia.org/wiki/Pok%C3%A9mon",), 
@@ -23,9 +31,20 @@ loader = WebBaseLoader(web_paths=("https://en.wikipedia.org/wiki/Pok%C3%A9mon",)
                            class_=('mw-content-container',)
                        )))
 text = loader.load()
-print(text)
+# print(text)
 
-### from pdf file
-loader = PyPDFLoader('pokemon.pdf')
-text = loader.load()
-print(text)
+
+## Data Transform
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+documents = text_splitter.split_documents(text)
+# print(documents[0])
+
+
+## Vector embeddings and store
+embeddings = OllamaEmbeddings(model="llama2")
+db = Chroma.from_documents(documents=documents, embedding=embeddings)
+
+query = 'who owns pokemon?'
+
+result = db.similarity_search(query)
+print(result[0].page_content)
